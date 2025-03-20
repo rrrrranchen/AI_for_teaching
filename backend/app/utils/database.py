6b3bce847ec5d922b1b750dd51bf9d75d7a2108a
rrrrranchen <1392510783@qq.com>
2025-03-20 15:38:27 +0800

    前端初始化

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -1,12 +1,11 @@
 
-
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 def init_db(app):
     """初始化数据库"""
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/eduai'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:767904rcnb@localhost:3306/eduai'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_POOL_SIZE'] = 10
     app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30
