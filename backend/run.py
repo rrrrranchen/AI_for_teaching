1621655727d9a40227c641995fb4a87efbe1f6a3
daliu <3193861477@qq.com>
2025-03-27 18:07:48 +0800

    允许前端访问图片资源

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,22 @@
 from app import create_app
+from flask import send_from_directory
 from flask_cors import CORS
+import os
 
 app = create_app()
 
-# 启用 CORS 支持
-CORS(app, origins=["http://localhost:8080"], supports_credentials=True)
+# 启用 CORS 支持（根据前端地址调整）
+CORS(app, resources={
+    r"/api/*": {"origins": "*"},  # API接口跨域
+    r"/static/*": {"origins": "*"}  # 静态资源跨域
+}, supports_credentials=True)
+
+# 添加静态资源路由
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    static_dir = os.path.join(root_dir, 'backend', 'static')
+    return send_from_directory(static_dir, filename)
 
 @app.route("/")
 def index():
