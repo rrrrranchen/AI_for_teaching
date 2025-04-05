4520f3ce6080bebcbedecee33964ed37bacc4211
daliu <3193861477@qq.com>
2025-04-06 00:27:29 +0800

    xiugai

--- backend/app/routes/question.py
+++ backend/app/routes/question.py
@@ -5,7 +5,6 @@ from werkzeug.security import check_password_hash
 from app.utils.database import db
 from app.models.question import Question
 from app.models.course import Course
-from app.services.demo import mock_ai_interface
 from app.models.user import User
 from app.models.courseclass import Courseclass
 from app.services.lesson_plan import generate_post_class_questions, generate_pre_class_questions
