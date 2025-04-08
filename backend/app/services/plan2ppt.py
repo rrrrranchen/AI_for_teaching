47f7c54d2f2f91474d9fe3d0e98f39ac689ae763
dingHom <1587270463@qq.com>
2025-04-08 16:35:31 +0800

    通过字典访问ppt模板

--- backend/app/services/plan2ppt.py
+++ backend/app/services/plan2ppt.py
@@ -241,15 +241,45 @@ def get_content(plan, structure):
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PPTTEMPLATE_FOLDER = os.path.join(project_root, 'static', 'template')
 PPT_FOLDER = os.path.join(project_root, 'static', 'uploads', 'ppts')
-filename = 'template.pptx'
+filename = 'template1.pptx'
 templatepath = os.path.join(PPTTEMPLATE_FOLDER, filename)
 structure = {
-    'template.pptx': [
+    'template1.pptx': [
         [3, 3],
         [3, 3, 3],
         [3, 4, 3],
         [3, 3, 3, 3],
         [3, 3, 3]
+    ],
+    'template2.pptx': [
+        [3],
+        [3],
+        [3, 3],
+        [3, 3, 3]
+    ],
+    'template3.pptx': [
+        [3, 4],
+        [4],
+        [4, 3],
+        [4]
+    ],
+    'template4.pptx': [
+        [5, 4, 4, 4],
+        [4, 5, 3],
+        [4, 4, 4, 6],
+        [3, 4, 3, 3]
+    ],
+    'template5.pptx': [
+        [3, 4, 4, 5],
+        [4, 5, 4, 4],
+        [3, 5, 4],
+        [3, 6, 3]
+    ],
+    'template6.pptx': [
+        [5, 4, 4, 4],
+        [4, 3, 3],
+        [3, 4],
+        [4, 4, 4, 4]
     ]
 }
 
