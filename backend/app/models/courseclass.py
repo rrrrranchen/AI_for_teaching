78097b48baf7e7f541efe4fa3cf930e529dd2041
daliu <3193861477@qq.com>
2025-06-25 15:02:28 +0800

    课程班是否公开

--- backend/app/models/courseclass.py
+++ backend/app/models/courseclass.py
@@ -11,10 +11,12 @@ class Courseclass(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     invite_code = db.Column(db.String(20), unique=True)  # 添加邀请码字段
     image_path = db.Column(db.String(500), nullable=True)  # 新增图片存储路径字段
+    is_public = db.Column(db.Boolean, default=False)  
     teachers = db.relationship('User', secondary=teacher_class, back_populates='teacher_courseclasses', lazy='joined')
     students = db.relationship('User', secondary=student_class, back_populates='student_courseclasses', lazy='joined')
     courses = db.relationship('Course', secondary=course_courseclass, back_populates='courseclasses', lazy='joined')
     student_reports = db.relationship('StudentAnalysisReport', back_populates='courseclass')
     class_reports = db.relationship('ClassAnalysisReport', back_populates='courseclass')
+
     def __repr__(self):
         return f'<Courseclass {self.name}>'
